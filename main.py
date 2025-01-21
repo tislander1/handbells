41ab828ba4bdab9895d7779a387ef3bb1d69f715
Tislander1 <blurryprojection@gmail.com>
2025-01-20 22:17:56 -0500

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import soundfile
 
 song, sample_rate = librosa.load('sound_test/11079__angstrom__d1.wav')
 song = song[0:len(song)//3]
-for note_int in range(-37, 1):
+num_notes = 37
+for note_int in range(-1*num_notes, 1):
     scaled_song = librosa.effects.pitch_shift(y=song, sr=sample_rate, bins_per_octave=12, n_steps=note_int, scale=True)
-    soundfile.write('sound_test/scaled_song'+ str(note_int) +'.wav', scaled_song, sample_rate)
+    soundfile.write('sound_test/note_num'+ str(num_notes+note_int) +'.wav', scaled_song, sample_rate)
