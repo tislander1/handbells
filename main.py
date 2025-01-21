07dcfa949b95f8bbd772617fd68ea0ff05daa433
Tislander1 <blurryprojection@gmail.com>
2025-01-20 22:13:33 -0500

    Update scaling of sound effects

--- main.py
+++ main.py
@@ -3,4 +3,6 @@ import soundfile
 
 song, sample_rate = librosa.load('sound_test/11079__angstrom__d1.wav')
 song = song[0:len(song)//3]
-soundfile.write('sound_test/stereo_file.wav', song, sample_rate)
+for note_int in range(-37, 1):
+    scaled_song = librosa.effects.pitch_shift(y=song, sr=sample_rate, bins_per_octave=12, n_steps=note_int, scale=True)
+    soundfile.write('sound_test/scaled_song'+ str(note_int) +'.wav', scaled_song, sample_rate)
