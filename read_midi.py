e2c9eae53250ef00321436d26df9ccc0ae93a1cc
Tislander1 <blurryprojection@gmail.com>
2025-01-24 09:53:46 -0500

    Add read_midi (does not work)

--- read_midi.py
+++ read_midi.py
@@ -2,4 +2,11 @@ from mido import MidiFile
 
 midi_song = MidiFile('sound_test/Fur Elise.mid', clip=True)
 
+division_val = 50
+song_list = []
+for trk_idx in range(len(midi_song.tracks)):
+    song_list.append([])
+    for msg in midi_song.tracks[trk_idx]:
+        if msg.type == 'note_on':
+            song_list[-1].append([msg.note, msg.time/division_val])
 x = 2
