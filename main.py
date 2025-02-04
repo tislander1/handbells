e669eae4f35fdf6508181d64ea3ccd72c8e7b1ac
Tislander1 <blurryprojection@gmail.com>
2025-02-03 19:28:24 -0500

    Store only the notes required for the song.

--- main.py
+++ main.py
@@ -18,9 +18,10 @@ def add_new_note(song, note, sample_idx):
         padded_note = np.concat( (np.zeros(start_of_note), note, np.zeros(len(song) - end_of_note)))
         return padded_note + song
 
-def make_song_from_array(notes, song_array, samples_between_notes):
+def make_song_from_array(note, song_array, samples_between_notes, zero_note):
     #song_array = [[0, 12, 400, 400, 7, 7]] # rest for undefined notes
-    sound_signal = notes[0][0:1]
+    notes = {}
+    sound_signal = note[0:0]
     for score in song_array:
         sample_idx = 0
         for note_idx in range(len(score)):
@@ -28,19 +29,19 @@ def make_song_from_array(notes, song_array, samples_between_notes):
 
             if isinstance(this_note_obj, int):  # simple note
                 note_value = this_note_obj + zero_note
-                if note_value in notes:
-                    sound_signal = add_new_note(sound_signal, notes[note_value], sample_idx)
-                else:
-                    sound_signal = add_new_note(sound_signal, 0*notes[0], sample_idx) # rest if there is no note defined
+                if note_value not in notes:
+                    notes[note_value] = librosa.effects.pitch_shift(y=note, sr=sample_rate, bins_per_octave=bins_per_octave, n_steps=note_value, scale=True)
+                sound_signal = add_new_note(sound_signal, notes[note_value], sample_idx)
                 sample_idx = sample_idx + int(samples_between_notes)
             elif isinstance(this_note_obj, list) and isinstance(this_note_obj[0], int):   #note that may be of a different length than a simple note
                 note_value = this_note_obj[0] + zero_note
-                if note_value in notes:
-                    sound_signal = add_new_note(sound_signal, notes[note_value], sample_idx)
-                else:
-                    sound_signal = add_new_note(sound_signal, 0*notes[0], sample_idx) # rest if there is no note defined
+                if note_value not in notes:
+                    notes[note_value] = librosa.effects.pitch_shift(y=note, sr=sample_rate, bins_per_octave=bins_per_octave, n_steps=note_value, scale=True)
+                sound_signal = add_new_note(sound_signal, notes[note_value], sample_idx)
                 sample_idx = sample_idx + int(samples_between_notes * this_note_obj[1])
-
+            elif isinstance(this_note_obj, list) and isinstance(this_note_obj[0], str): #rests 
+                sound_signal = add_new_note(sound_signal, 0*notes[0], sample_idx)
+                sample_idx = sample_idx + int(samples_between_notes * this_note_obj[1])
             elif isinstance(this_note_obj, list) and isinstance(this_note_obj[0], tuple):
                 note_values = [n[0] + zero_note for n in this_note_obj]
                 note_time_values = [t[1] for t in this_note_obj]
@@ -48,11 +49,10 @@ def make_song_from_array(notes, song_array, samples_between_notes):
                 max_note_length = int(max([note_time_values[i] + delay_values[i] for i in range(len(note_time_values))]) * samples_between_notes)
                 for ix in range(len(note_values)):
                     note_value = note_values[ix]
-                    if note_value in notes:
-                        delay = int(delay_values[ix] * samples_between_notes)
-                        sound_signal = add_new_note(sound_signal, notes[note_value], sample_idx + delay)
-                    else:
-                        sound_signal = add_new_note(sound_signal, 0*notes[0], sample_idx + delay)
+                    if note_value not in notes:
+                        notes[note_value] = librosa.effects.pitch_shift(y=note, sr=sample_rate, bins_per_octave=bins_per_octave, n_steps=note_value, scale=True)
+                    delay = int(delay_values[ix] * samples_between_notes)
+                    sound_signal = add_new_note(sound_signal, notes[note_value], sample_idx + delay)
                 sample_idx = sample_idx + max_note_length
                 
             # if isinstance(this_note_obj, int):  # simple note
@@ -61,23 +61,23 @@ def make_song_from_array(notes, song_array, samples_between_notes):
             #     sample_idx = sample_idx + int(samples_between_notes * this_note_obj[1])
     return sound_signal
 
-note, sample_rate = librosa.load('sound_test/418424__johnnyguitar01__tubular-bell.wav')
-note = note[0:len(note)] #trim off quiet following the note
-num_notes_lower = 37
-num_notes_higher = 13
-first_note = 0
-last_note = num_notes_lower + num_notes_higher - 1
+input_sound_file = 'sound_test/418424__johnnyguitar01__tubular-bell.wav'
+output_sound_file = 'sound_test/output.wav'
 
+bins_per_octave = 12
+note_trim_fraction = 1.0    # can trim off end of note if desired (just make this less than 1.0)
 time_between_notes = 0.25  # you can choose the shortest note (e.g. an eighth note) to avoid fractions in the song array,
                             # though it's not required
-zero_note = 20
+zero_note = -10
+
+note, sample_rate = librosa.load(input_sound_file)
+
+if note_trim_fraction > 1.0:
+    note_trim_fraction = 1.0
+note = note[0: int(note_trim_fraction * len(note))] #trim off quiet following the note
+
 
 samples_between_notes = time_between_notes * sample_rate
-
-notes = {}
-for note_int in range(-1*num_notes_lower, num_notes_higher):
-    scaled_note= librosa.effects.pitch_shift(y=note, sr=sample_rate, bins_per_octave=12*1000, n_steps=note_int*1000, scale=True)
-    notes[note_int + num_notes_lower] = scaled_note
 
 # song_array = [[0, 2, 4, 5, 7, 9, 11, 12], [2, 4, 5, 7, 9, 11, 12, 14], [4, 5, 7, 9, 11, 12, 14, 16]]  # do re mi, plus friends
 
@@ -102,8 +102,8 @@ song_array = [
      []
      ]
 
-sound_signal = make_song_from_array(notes, song_array, samples_between_notes)
+sound_signal = make_song_from_array(note=note, song_array=song_array, samples_between_notes=samples_between_notes, zero_note=zero_note)
 
-soundfile.write('sound_test/output.wav', sound_signal, sample_rate)
+soundfile.write(output_sound_file, sound_signal, sample_rate)
 print('Done! ' + str(datetime.now()))
 x = 2
